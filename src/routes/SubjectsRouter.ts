30ae9de014a2e7356ed67ff331ae9c697894adf7
Khoa Le <5865258+KhoaLeTea@users.noreply.github.com>
2024-06-02 23:21:10 -0400

    Subject get by category (#15)
    * Add SubjectService.ts file.
    
    * Update SubjectService to get subjects by a categoryId.

--- src/routes/SubjectsRouter.ts
+++ src/routes/SubjectsRouter.ts
@@ -1,20 +1,34 @@
 import express from 'express';
 import pg from '../database/knex';
 import { Subject } from '@src/models/Subjects';
+import SubjectService from '@src/services/SubjectService';
 
 
 const subjectRouter = express.Router();
 
 // Define routes
 subjectRouter.get('/', async function (req, res) {
-  const subjects: Subject[] = await pg('subjects').select('*');
+  const subjects: Subject[] = await SubjectService.getAllSubjects();
+
+
   res.send(subjects);
 });
 
 subjectRouter.get('/:id', async function (req, res) {
-  const subjectId = req.params.id;
-  const subject: Subject = await pg('subjects').where({id: subjectId}).first('*');
+  const subjectId = parseInt(req.params.id);
+  const subject: Subject = await SubjectService.getBySubjectId(subjectId);
+
   res.send(subject);
 });
+
+// Example route: localhost:3000/subjects?categoryId=1
+subjectRouter.get('/byCategory/:categoryId', async function (req, res) {
+  console.log("by catgegory")
+  const categoryId: number = parseInt(req.params.categoryId);
+
+  const subjectsByCategory: Subject[]= await SubjectService.getByCategory(categoryId);
+
+  res.send(subjectsByCategory)
+})
 
 export default subjectRouter;
